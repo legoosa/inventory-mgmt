0ca026b28039d2004f1e643d0701872c2bdfff93
Suresh Kumar Devaraj <dpsures@gmail.com>
2018-09-15 21:50:01 +0530

    eureka client discovery added

--- user-db-api/src/main/java/org/inventory/mgmt/RunUserDbService.java
+++ user-db-api/src/main/java/org/inventory/mgmt/RunUserDbService.java
@@ -2,10 +2,12 @@ package org.inventory.mgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEurekaClient
 public class RunUserDbService {
 
 	public static void main(String[] args) {
